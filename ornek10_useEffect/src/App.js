e6fd97f5dfc870be33e6db74496a9e5b14ee5363
bilalcanbulat <bilalcanbulat19@gmail.com>
2021-06-09 22:20:59 +0300

    guncel

--- ornek10_useEffect/src/App.js
+++ ornek10_useEffect/src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react'
 import UseEffect01 from './UseEffect01';
+import UseEffectFetch from './UseEffectFetch';
 
 function App() {
   return (
     <div>
-    <UseEffect01 />
+    {/* <UseEffect01 /> */}
+    <UseEffectFetch />
       
     </div>
   )
